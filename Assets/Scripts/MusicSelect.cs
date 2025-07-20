93c947e6498e75f333829e8600aea0535b0ed9b6
bluecircle6437 <bluecircle6437@gmail.com>
2025-07-20 20:32:28 +0900

    곡 선택 화면 스크립팅 준비

---
+++ Assets/Scripts/MusicSelect.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MusicSelect : MonoBehaviour
+{
+    public Image cover;
+    public TextMeshProUGUI songName;
+    public TextMeshProUGUI songArtist;
+    public ScrollRect songScroll;
+    public ScrollRect category;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
